122dd65e9692ba77f0c5522511cfc7163ad4d379
System Administrator <root@tim.home>
2019-11-28 20:34:51 +0000

    completed chat_app

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const io = require('socket.io')(3000)
 
+const users = {}
+
+
 io.on('connection', socket => {
-  console.log('new user');
-  socket.emit('chat-message', 'Hiya world')
+socket.on('new-user', name => {
+  users[socket.id] = name
+  socket.broadcast.emit('user-connected, name')
+})
+
+
+  socket.on('send-chat-message', message => {
+  socket.broadcast.emit('chat-meassage', {message: message, name:
+  users[socket.id]})
+  })
+
+
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user-disconneced, users[socket.id]')
+    delete users[socket.id]
+  })
 })
